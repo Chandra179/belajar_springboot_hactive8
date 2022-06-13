a16105ee307fc5fc8c826917ab945fa044336163
Management Trainee Technology Head Office (Chandra) <095047@bfi.co.id>
2022-06-14 06:41:03 +0700

    ad

---
+++ sesi_01/SwitchOld.java
@@ -0,0 +1,24 @@
+public class SwitchOld {
+	public static void main (String args[]) {
+		String result = "";
+		String day = "M";
+		switch (day)
+		{
+			case "M":
+			case "W":
+			case "F":
+			{
+				result = "MWF";
+			}
+			case "T":
+			case "TH":
+			case "S":
+			{
+				result = "TTS";
+				break;
+			}
+		};
+		System.out.println("Old Switch result:");
+		System.out.println(result);
+	}
+}
