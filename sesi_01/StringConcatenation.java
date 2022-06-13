562cf29c6558b210b5d052cee5fe6bf4863ce9a5
Management Trainee Technology Head Office (Chandra) <095047@bfi.co.id>
2022-06-13 23:07:47 +0700

    add

---
+++ sesi_01/StringConcatenation.java
@@ -0,0 +1,8 @@
+public class StringConcatenation{
+    public static void main (String args[]) {
+		String namaDepan = "chandra";
+        String namaBelakang = ".";
+
+        System.out.println(namaDepan + " " + namaBelakang);
+	}
+}
