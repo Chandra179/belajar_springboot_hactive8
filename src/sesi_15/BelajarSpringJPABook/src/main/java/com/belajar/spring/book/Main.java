e0b13675223c33515eed18ad3bd35f0786396de1
Management Trainee Technology Head Office (Chandra) <095047@bfi.co.id>
2022-06-22 22:03:11 +0700

    'add'

--- src/sesi_15/BelajarSpringJPABook/src/main/java/com/belajar/spring/book/Main.java
+++ src/sesi_15/BelajarSpringJPABook/src/main/java/com/belajar/spring/book/Main.java
@@ -27,52 +27,59 @@ public class Main implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-//		Book book1 = new Book();
-//		book1.setTitle("Belajar Spring Boot 1");
-//		book1.setWriter("Wilson aja");
-//		book1.setIsbn("IS-909190");
-//
-//		Book book2 = new Book();
-//		book2.setTitle("Belajar Spring Boot 2");
-//		book2.setWriter("Kevin");
-//		book2.setIsbn("IS-909089");
-//
-//		bookRepository.save(book1);
-//		bookRepository.save(book2);
+		Book book1 = new Book();
+		book1.setTitle("Belajar Spring Boot 1");
+		book1.setWriter("Wilson aja");
+		book1.setIsbn("IS-909190");
 
+		Book book2 = new Book();
+		book2.setTitle("Belajar Spring Boot 2");
+		book2.setWriter("Kevin");
+		book2.setIsbn("IS-909089");
+
+		Book book3 = new Book();
+		book3.setTitle("Belajar Spring Boot 3");
+		book3.setWriter("Jackson");
+		book3.setIsbn("IS-351089");
+
+		bookRepository.save(book1);
+		bookRepository.save(book2);
+		bookRepository.save(book3);
+
+		
 		final String title = "Belajar Spring Boot 1";
 		List<Book> byTitle = bookRepository.findByTitle(title);
-		byTitle.forEach(x -> System.out.println(x.getTitle()));
 		logger.log(Level.INFO, "Title : " + byTitle);
+
 
 		List<Book> books = bookRepository.findAll();
 		logger.log(Level.INFO, "Books : " + books);
 
+
 		final String writer = "Wilson aja";
 		List<Book> byWriter = bookRepository.findAllByWriter(writer);
-		byWriter.forEach(x -> System.out.println(x.getWriter()));
 		logger.log(Level.INFO, "Writer : " + byWriter);
+
 
 		final String isbn = "IS-909089";
 		List<Book> byIsbn = bookRepository.findByIsbn(isbn);
-		byIsbn.forEach(x -> System.out.println(x.getIsbn()));
 		logger.log(Level.INFO, "ISBN : " + byIsbn);
 
+
 		List<Book> nativeBooksQuery = bookRepository.findAllQueryNative();
-		nativeBooksQuery.forEach(x -> System.out.println(x.getTitle() + " " + x.getWriter()));
 		logger.log(Level.INFO, "Book by native query : " + nativeBooksQuery);
+
 
 		final String nativeWriter = "Kevin";
 		List<Book> byNativeWriter = bookRepository.findAllByWriterQueryNative(nativeWriter);
-		byNativeWriter.forEach(x -> System.out.println(x.getTitle() + " " + x.getWriter()));
 		logger.log(Level.INFO, "Writer by native query : " + byNativeWriter);
+
 
 		BookCategory bookCategory = bookCategoryRepository.save(
 				new BookCategory("Programming",
 				new Book("Java 1", "Teten N.", "SEI92002"), 
 				new Book("Java 2", "Teten N.", "UEOEI829"))
 		);
-
 		logger.log(Level.INFO, "bookCategory : " + bookCategory);
 	}
 
