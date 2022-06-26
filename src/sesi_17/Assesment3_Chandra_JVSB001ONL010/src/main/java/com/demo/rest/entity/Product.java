e1a79af0ae976867803a8a5f4915a3ce1e7ea54e
Management Trainee Technology Head Office (Chandra) <095047@bfi.co.id>
2022-06-26 10:23:45 +0700

    'add'

--- src/sesi_17/Assesment3_Chandra_JVSB001ONL010/src/main/java/com/demo/rest/entity/Product.java
+++ src/sesi_17/Assesment3_Chandra_JVSB001ONL010/src/main/java/com/demo/rest/entity/Product.java
@@ -8,12 +8,10 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Data;
-import lombok.ToString;
 
 @Entity
 @Table(name = "tb_product")
 @Data
-@ToString
 public class Product {
 	
 	@Id
@@ -28,37 +26,4 @@ public class Product {
 
 	@Column(name = "hargaJual")
 	private int hargaJual;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getHargaBeli() {
-		return hargaBeli;
-	}
-
-	public void setHargaBeli(int hargaBeli) {
-		this.hargaBeli = hargaBeli;
-	}
-
-	public int getHargaJual() {
-		return hargaJual;
-	}
-
-	public void setHargaJual(int hargaJual) {
-		this.hargaJual = hargaJual;
-	}
-
 }
--- src/sesi_17/Assesment3_Chandra_JVSB001ONL010/src/main/java/com/demo/rest/repository/ProductRepository.java
+++ src/sesi_17/Assesment3_Chandra_JVSB001ONL010/src/main/java/com/demo/rest/repository/ProductRepository.java
@@ -5,8 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.demo.rest.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
 	Product findProductByName(String name);
 
 	Product findProductById(Long id);
-
 }
